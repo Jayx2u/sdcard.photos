33feac77195b1e51a8f45c9db577ed60a1331b33
Jay <141212189+jayx2u@users.noreply.github.com>
2024-11-06 23:05:07 +1100

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react"
+
 export const metadata: Metadata = {
   title: "sdcard.photos",
   description: "A photo blog for a group of friends passionate about photography.",
@@ -15,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className='bg-black'>
           {children}
+          <Analytics />
       </body>
     </html>
 );
