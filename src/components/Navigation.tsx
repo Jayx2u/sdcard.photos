5a0f9fa190dbcb2837004f9ce25a9a374e8bace4
Jay <141212189+jayx2u@users.noreply.github.com>
2024-11-21 13:56:48 +1100

    Fix ESLint warning

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react';
 import Link from 'next/link';
-import { Moon, Sun, Menu } from 'lucide-react';
+import { Moon, Sun, /*Menu*/ } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from "@/components/ui/button";
